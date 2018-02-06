779346faedfa276aee39930fbfa8fdbb78f1b72d
brucelt1993 <brucelt_1993@163.com>
2018-02-06 18:14:48 +0800

    2018-02-06  spring4实战第五章:thymeleaf   add by bruce

--- src/main/java/spittr/web/WebConfig.java
+++ src/main/java/spittr/web/WebConfig.java
@@ -4,16 +4,19 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
 import org.springframework.web.servlet.view.tiles3.TilesViewResolver;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+import org.thymeleaf.templateresolver.TemplateResolver;
 
 @Configuration
 @EnableWebMvc
@@ -35,10 +38,10 @@ public class WebConfig extends WebMvcConfigurerAdapter {
      * 配置TilesViewResolver视图解析器
      * @return
      */
-   @Bean
+   /*@Bean
    public ViewResolver viewResolver(){
        return new TilesViewResolver();
-   }
+   }*/
 
     /**
      * 配置TilesConfigurer来解析tile定义
@@ -52,6 +55,42 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         /* 启用刷新功能 */
         tiles.setCheckRefresh(true);
         return tiles;
+    }
+
+    /**
+     * Thymeleaf 视图解析器
+     * @param templateEngine
+     * @return
+     */
+    @Bean
+    public ViewResolver viewResolver(SpringTemplateEngine templateEngine) {
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine);
+        return viewResolver;
+    }
+
+    /**
+     * Thymeleaf 模板引擎
+     * @param templateResolver
+     * @return
+     */
+    @Bean
+    public SpringTemplateEngine templateEngine(TemplateResolver templateResolver) {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+        return templateEngine;
+    }
+    /**
+     * Thymeleaf 模板解析器
+     * @return
+     */
+    @Bean
+    public TemplateResolver templateResolver() {
+        TemplateResolver templateResolver = new ServletContextTemplateResolver();
+        templateResolver.setPrefix("/WEB-INF/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML5");
+        return templateResolver;
     }
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
